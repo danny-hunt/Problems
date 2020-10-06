dc64401e460a9f9baeed3a59a6b1cc90cec85577
Daniel Hunt <dansinh64@gmail.com>
2020-10-06 14:36:56 +0100

    add h_index problem

---
+++ h_index/h_index.py
@@ -0,0 +1,30 @@
+"""
+In academia, the h-index is a metric used to calculate the impact of a researcher's papers. 
+It is calculated as follows:
+
+A researcher has index h if at least h of her N papers have h citations each. 
+If there are multiple h satisfying this formula, the maximum is chosen.
+
+For example, suppose N = 5, and the respective citations of each paper are [4, 3, 0, 1, 5]. 
+Then the h-index would be 3, since the researcher has 3 papers with at least 3 citations.
+
+Given a list of paper citations of a researcher, calculate their h-index.
+"""
+import copy
+
+citations_list = [4, 3, 0, 1, 5, 6, 7]
+
+
+def h_index(citations_list):
+    c = copy.copy(citations_list)
+    c.sort()
+    print(c)
+    current_h = 0
+    number_of_papers = len(c)
+    for index, value in enumerate(c):
+        if value <= number_of_papers - index:
+            current_h = value
+    return current_h
+
+print(h_index(citations_list))
+
