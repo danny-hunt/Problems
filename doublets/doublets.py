cfe37cc1a14c6f5723ec633372068e9780a7b8e8
Daniel Hunt <dansinh64@gmail.com>
2020-10-07 15:44:51 +0100

    musings

--- doublets/doublets.py
+++ doublets/doublets.py
@@ -18,19 +18,30 @@ return null as there is no possible transformation from dog to cat.
 start = "dog"
 end = "cat"
 dictionary = {"dot", "dop", "dat", "cat"}
+start_word_set = [start]
+checked_words = set(start_word_set[0])
+#dictionary.
 
-def list_one_off(start_word, dictionary):
+
+def list_one_off(start_word_set, dictionary, checked_words = checked_words):
     one_step_words = []
-    for word in dictionary:
-        count = 0
-        for x in range(len(word)):
-            if word[x] != start_word[x]:
-                count += 1
-        if count == 1:
-            one_step_words.append(word)
+    for starting_word in start_word_set:
+        for possible_word in dictionary:
+            count = 0
+            for letter in range(len(possible_word)):
+                if starting_word[letter] != possible_word[letter]:
+                    count += 1
+            if count == 1 and possible_word not in checked_words:
+                one_step_words.append(possible_word)
     return one_step_words
 
-print(list_one_off("dog", dictionary))
+print(list_one_off(start_word_set, dictionary))
+
+def update_checked_words(newly_checked_words, checked_words_set):
+    checked_words_set.add_contents_of(one_step_words)
+
+#set might need to be replaced by a dict so that we can track the path to the answer
+#atm all we can do is check whether there is a path
 
 
         
--- ghost/ghost.py
+++ ghost/ghost.py
@@ -14,11 +14,52 @@ such that with optimal play they cannot lose.
 For example, if the dictionary is ["cat", "calf", "dog", "bear"], 
 the only winning start letter would be b.
 """
+
+import string
+import re
+
+"""
+for each letter in the alphabet, see whether that letter gives P1 a forced win
+a letter gives a forced win iff there is no forced win for the second player in response 
+AND there is a valid word with that starting letter sequence
+
+function: determine winner from state A = current string + indication of who is to play
+
+say player one = 0
+    player two = 1
+"""
+
+alphabet = list(string.ascii_lowercase)
+dictionary = ["cat", "calf", "dog", "bear"]
+dictionary.sort()
+dictionary_with_lengths = [ [x, len(x)] for x in dictionary]
+print(dictionary_with_lengths)
+
+
+def word_exists(string, dictionary = dictionary):
+    # create regex pattern for string + [a-z]+ and check existence in dictionary
+    pass
+
+
+
+def who_wins(to_play = 0, string = ''):
+    existing_length = len(string)
+    if not word_exists(string):
+        print(f'player {to_play} wins through lack of word')
+        return 0
+    
+    #for
+    
+
+    
+
+
+
 import string
 
 dictionary = ["cat", "calf", "dog", "bear"]
 dictionary.sort()
 
 alphabet = list(string.ascii_lowercase)
-for letter in alphabet:
-    if 
+#for letter in alphabet:
+#    if 
