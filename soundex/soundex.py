037ee33284b39e6fd960c34079d1eaf87269b691
Daniel Hunt <dansinh64@gmail.com>
2020-10-08 10:09:31 +0100

    initial soln for soundex

--- soundex/soundex.py
+++ soundex/soundex.py
@@ -37,11 +37,30 @@ def replace_consonants_with_digits(name, mapping=mapping):
         if letter in consonants:
             name[index] = mapping[letter]
             print(letter)
-    return str(name) # this needs to actually be string for the name
+    return name # this needs to actually be string for the name
+
+def truncate_repeated_letters(name):
+    truncated_name = [value for index, value in enumerate(name) if value != name[index-1]]
+    return truncated_name
+
+
 
 
 def soundex(name):
-    first_letter = name[0]
+    name = name.lower()
+    first_letter = str(name[0])
     name = replace_consonants_with_digits(name)
+    truncated_name = truncate_repeated_letters(name)
+    del(truncated_name[0])
+    vowelless_name = [letter for letter in truncated_name if letter not in vowels]
+    print(vowelless_name)
+    print(first_letter)
+    output = first_letter + ''.join(str(number) for number in vowelless_name[:3])
+    while len(output) < 4:
+        output += '0'
+    output = output.upper()
+    return output
 
 print(replace_consonants_with_digits('Daniel'))
+print(soundex('Damniel'))
+print(soundex('alexandrpiu'))
