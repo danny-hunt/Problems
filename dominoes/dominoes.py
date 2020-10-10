03b5cf8398e66dfaca6b16be7bafca054fb150a4
JerryDot <dansinh64@gmail.com>
2020-10-10 22:36:55 +0100

    solve dominoes

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -14,8 +14,36 @@ For example, given the string .L.R....L, you should return LL.RRRLLL
 Given the string ..R...L.L, you should return ..RR.LLLL
 """
 
-dominoes = list("..R...L.L")
-print(dominoes)
 
-def first_approach():
-    pass
+def calculate(dominoes):
+    index = 0
+    current_state = dominoes[index]
+    to_resolve = []
+    for index in range(len(dominoes)):
+        if dominoes[index] == '.':
+            to_resolve.append(index)
+        if dominoes[index] == 'R':
+            current_state = 'R'
+            to_resolve = [index]
+        if dominoes[index] == 'L':
+            if current_state == '.':
+                for ii in to_resolve:
+                    dominoes[ii] = 'L'
+            if current_state == 'R':
+                for i, value in enumerate(to_resolve):
+                    if i < len(to_resolve) // 2:
+                        dominoes[value] = 'R'
+                    if i > len(to_resolve) // 2:
+                        dominoes[value] = 'L'
+            to_resolve = []
+            current_state = '.'
+    return ''.join(dominoes)
+
+
+dominoes = list(".LR...L.L..R...L.L..R...L.L..R...L.L")
+print(calculate(dominoes))
+
+
+
+
+
