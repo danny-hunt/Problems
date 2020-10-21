fac507877dab5d34d1518598e204086c967d65d1
JerryDot <dansinh64@gmail.com>
2020-10-21 06:51:07 +0100

    add fizzbuzz

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,60 @@
+def fizzbuzz_naive(n):
+    for number in range(1, n+1):
+        output = ""
+        if number % 3 == 0:
+            output += "fizz"
+        if number % 5 == 0:
+            output += "buzz"
+        if len(output) == 0:
+            output += str(number)
+        print(output)
+
+
+def fizzbuzz_recurse(n, array=[]):
+    if n % 3 == 0:
+        if n % 5 == 0:
+            array.append("fizzbuzz")
+        else:
+            array.append("fizz")
+    elif n % 5 == 0:
+        array.append("buzz")
+    else:
+        array.append(n)
+
+    if n == 1:
+        for entry in reversed(array):
+            print(entry)
+    else:
+        fizzbuzz_recurse(n-1, array)
+
+
+import json
+import random
+import urllib.request
+
+
+def fizzbuzz3(n, *args):
+    words = ["fizz", "buzz", "wang", "bang", "cough"]
+    factors = [factor for factor in args]
+    factor_words = {}
+    url = urllib.request.urlopen("https://raw.githubusercontent.com/sindresorhus/mnemonic-words/master/words.json")
+    word_list = json.loads(url.read())
+    for index, factor in enumerate(factors):
+        if index < 5:
+            factor_words[str(factor)] = words[index]
+        else:
+            factor_words[str(factor)] = random.choice(word_list)
+
+    for number in range(1,n+1):
+        output = ""
+        for factor in factors:
+            if number % factor == 0:
+                output = output + factor_words[str(factor)]
+        if len(output) == 0:
+            output += str(number)
+        print(output)
+
+
+fizzbuzz_naive(100)
+fizzbuzz_recurse(100)
+fizzbuzz3(100, 2, 3, 5, 7, 11, 6)
---
+++ sudoku_solver/sudoku_solver.py
@@ -0,0 +1,41 @@
+import numpy as np
+import json
+import pprint
+import copy
+
+with open('sudoku.json') as json_file:
+    list_of_puzzles = json.load(json_file)['puzzle']
+
+def solve(sudoku):
+    completed_sudoku = 5
+
+    return completed_sudoku
+
+def print_sudoku(board):
+    board_for_printing = copy.copy(board)
+    for index_r, row in enumerate(board_for_printing):
+        for index_c, column in enumerate(row):
+            if column == 0:
+                board_for_printing[index_r][index_c] = ' '
+    print('-' + '----'*9)
+    for index, row in enumerate(board_for_printing):
+        print(f'| {row[0]}   {row[1]}   {row[2]} | {row[3]}   {row[4]}   {row[5]} | {row[6]}   {row[7]}   {row[8]} |')
+        if index % 3 == 2:
+            print('|' + '-----------|'*3)
+        else:
+            print('|   +   +   '*3 + '|')
+
+
+def check_solution(board):
+    possible_values = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    for row in board:
+        for value in possible_values:
+            if value not in row:
+                return False
+    return True
+
+
+print(list_of_puzzles)
+pprint.pprint(list_of_puzzles[0])
+print_sudoku(list_of_puzzles[0])
+print(check_solution(list_of_puzzles[0]))
