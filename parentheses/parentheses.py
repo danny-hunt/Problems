ea22b2ceed7c554fd7cef27bf349c0ff39b10f0a
JerryDot <dansinh64@gmail.com>
2020-10-23 22:46:00 +0100

    add parentheses puzzle

--- parentheses/parentheses.py
+++ parentheses/parentheses.py
@@ -6,3 +6,30 @@ to make the string valid (i.e. each open parenthesis is eventually closed).
 For example, given the string "()())()", you should return 1.
 Given the string ")(", you should return 2, since we must remove all of them.
 """
+
+"""
+) <-- always removed
+ ((()(()
+01232343
+
+"""
+
+
+def count_removal(parentheses):
+    levels_deep = 0
+    backwards_facing_removed = 0
+    for parenthesis in parentheses:
+        if parenthesis == "(":
+            levels_deep += 1
+        if parenthesis == ")":
+            if levels_deep == 0:
+                backwards_facing_removed += 1
+            else:
+                levels_deep -= 1
+    return levels_deep + backwards_facing_removed
+
+test_string = ")(("
+assert count_removal(test_string) == 3
+another_test_string = "()))()()((()()((((()()()()))))))"
+assert count_removal(another_test_string) == 2
+
