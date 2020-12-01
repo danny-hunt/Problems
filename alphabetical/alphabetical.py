0b48fe70456d21df813fe65220a49b9d08165142
JerryDot <dansinh64@gmail.com>
2020-12-01 10:57:29 +0000

    solve alpha

--- alphabetical/alphabetical.py
+++ alphabetical/alphabetical.py
@@ -4,6 +4,31 @@ Write a program that returns the correct order of letters in this language.
 
 For example, given ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz'],
 you should return ['x', 'z', 'w', 'y'].
+
+abc abd eaf
+
 """
 
-from collections import deque
+
+def compare_words(word_one, word_two, letter_order):
+    for i in range(len(word_one)):
+        if word_one[i] != word_two[i]:
+            letter_order.add((word_one[i], word_two[i]))
+            break
+
+
+base_dictionary = ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz']
+
+
+def determine_order(dictionary):
+    order = set()
+    for i in range(len(base_dictionary) - 1):
+        compare_words(base_dictionary[i], base_dictionary[i+1], order)
+
+    # order now gives the pairwise comparisons
+    # these still need to be turned into an example of a topological ordering
+
+
+determine_order(base_dictionary)
+
+
