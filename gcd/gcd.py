6c0a1693d6c249029b89b3e99ef6a711c7bf94d6
JerryDot <dansinh64@gmail.com>
2021-01-07 20:12:41 +0000

    add linked_list functionality

---
+++ gcd/gcd.py
@@ -0,0 +1,15 @@
+"""
+Given n numbers, find the greatest common denominator between them.
+
+For example, given the numbers [42, 56, 14], return 14.
+"""
+
+number_list = [42, 56, 14, 28]
+
+def find_gcd(numbers):
+
+
+
+
+def find_gcd_pair(number_one, number_two):
+    
--- linked_list_implementation.py
+++ linked_list_implementation.py
@@ -29,6 +29,9 @@ class LinkedList:
             output_string = output_string + str(visited_node.value)
         return output_string
 
+    def go_to_next_node(self):
+        self.last_node_touched = self.last_node_touched.next_node
+
     def reverse(self):
         previous_node = None
         while self.last_node_touched.next_node is not None:
@@ -38,6 +41,23 @@ class LinkedList:
             self.last_node_touched = next_node
 
         self.last_node_touched.next_node = previous_node
+
+    def rotate(self, k):
+        first_node = self.last_node_touched
+        current_node = self.last_node_touched
+        count = 0
+        while current_node.next_node:
+            current_node = current_node.next_node
+            count += 1
+        current_node.set_next_node(first_node)
+        count -= k
+        while count >= 0:
+            current_node = current_node.next_node
+            count -= 1
+        next_node = current_node.next_node
+        current_node.next_node = None
+        self.last_node_touched = next_node
+
 
 
 def reverse(linked_list):
@@ -59,3 +79,19 @@ print(ell_ell)
 
 function_reversed_list = reverse(ell_ell)
 print(function_reversed_list)
+
+
+"""
+Given a linked list and a positive integer k, rotate the list to the right by k places.
+
+For example, given the linked list 7 -> 7 -> 3 -> 5 and k = 2, it should become 3 -> 5 -> 7 -> 7.
+
+Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and k = 3, it should become 3 -> 4 -> 5 -> 1 -> 2.
+
+"""
+emm_emm = LinkedList(1,2,3,4,5,6)
+emm_emm.rotate(3)
+
+print(emm_emm)
+emm_emm.rotate(2)
+print(emm_emm)
