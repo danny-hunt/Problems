786276978185d2f44380d44cfe904706beaaad74
JerryDot <dansinh64@gmail.com>
2021-01-08 23:12:56 +0000

    add puzzle

---
+++ limited_dijkstra/limited_dijkstra.py
@@ -0,0 +1,17 @@
+"""
+Consider a user who is willing to take up to k connections from their origin city A to their destination B.
+Find the cheapest fare possible for this journey and print the itinerary for that journey.
+
+For example, our traveler wants to go from JFK to LAX with up to 3 connections, and our input flights are as follows:
+
+[
+    ('JFK', 'ATL', 150),
+    ('ATL', 'SFO', 400),
+    ('ORD', 'LAX', 200),
+    ('LAX', 'DFW', 80),
+    ('JFK', 'HKG', 800),
+    ('ATL', 'ORD', 90),
+    ('JFK', 'LAX', 500),
+]
+Due to some improbably low flight prices, the cheapest itinerary would be JFK -> ATL -> ORD -> LAX, costing $440
+"""
