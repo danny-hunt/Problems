ebabb112d2a7f13c6093609f6ceaf14fda04c265
JerryDot <dansinh64@gmail.com>
2021-02-03 19:14:35 +0000

    add stock_prices and binary heap implementation

---
+++ binary_heap.py
@@ -0,0 +1,43 @@
+from random import randint
+
+
+class BinaryHeap:
+
+    def __init__(self, value):
+        self.value = value
+        self.children = [None, None]
+
+    def __str__(self):
+        return_string = ""
+        return_string += str(self.value)
+        for child in self.children:
+            return_string += str(child)
+        return return_string
+
+    @staticmethod
+    def __add_node(new_value, heap, parent=None):
+        if new_value > heap.value:
+            new_node = BinaryHeap(new_value)
+            new_node.children[0] = heap
+            if parent is not None:
+                parent.children[parent.children.index(heap)] = new_node
+            return new_node
+        else:
+            if None in heap.children:
+                heap.children[heap.children.index(None)] = BinaryHeap(new_value)
+                return heap
+            else:
+                to_add = heap.children[randint(0, 1)]
+                return to_add.__add_node(new_value, to_add, heap)
+
+    def add(self, new_value):
+        return self.__add_node(new_value, self)
+
+
+x = BinaryHeap(5)
+x.add(3)
+x.add(4)
+x.add(2)
+x.add(1)
+x.add(2)
+print(x)
---
+++ trading_stocks/trading_stocks.py
@@ -0,0 +1,28 @@
+
+"""
+Given a array of numbers representing the stock prices of a company in chronological order,
+write a function that calculates the maximum profit you could have made from buying and selling that stock once.
+You must buy before you can sell it.
+
+For example, given [9, 11, 8, 5, 7, 10], you should return 5,
+since you could buy the stock at 5 dollars and sell it at 10 dollars.
+"""
+from random import randint
+from typing import List
+
+
+stock_prices = [randint(4, 20) for _ in range(10)]
+
+def best_return(prices: List[int]) -> int:
+    current_lowest = stock_prices[0]
+    current_max_return = 0
+    for cost in prices:
+        if cost < current_lowest:
+            current_lowest = cost
+        else:
+            if cost - current_lowest > current_max_return:
+                current_max_return = cost - current_lowest
+    return current_max_return
+
+print(stock_prices)
+print(best_return(stock_prices))
